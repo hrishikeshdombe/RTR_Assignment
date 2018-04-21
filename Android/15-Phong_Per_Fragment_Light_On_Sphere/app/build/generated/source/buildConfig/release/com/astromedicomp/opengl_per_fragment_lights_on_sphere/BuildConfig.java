86f3992f5ea9ad69ba0477d280274cb13692e496
hrishikeshdombe <hrishikeshdombe@gmail.com>
2018-04-21 18:49:42 +0530

    Android Programs

---
+++ Android/15-Phong_Per_Fragment_Light_On_Sphere/app/build/generated/source/buildConfig/release/com/astromedicomp/opengl_per_fragment_lights_on_sphere/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.astromedicomp.opengl_per_fragment_lights_on_sphere;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.astromedicomp.opengl_per_fragment_lights_on_sphere";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
