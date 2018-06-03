2fa4c29f91545e1f1e9eba3ae38bd5f63b43f45f
unknown <hrishikeshdombe@gmail.com>
2018-06-03 13:11:17 +0530

    Commit For Updated Android,GLUT,MacOS,Native,PP_Windows,WebGL

---
+++ Android/My_Android_Projects/temp/app/build/generated/source/buildConfig/release/com/astromedicomp/winvm_gles_triangle_ortho/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.astromedicomp.winvm_gles_triangle_ortho;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.astromedicomp.winvm_gles_triangle_ortho";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
